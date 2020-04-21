f217d3f0be1d109fe98a66d11498861dd0f80608
Jacek Grochowina <jacek99@vp.pl>
2020-04-21 15:05:02 +0200

    firebase config

---
+++ src/js/firebase-config.js
@@ -0,0 +1,18 @@
+
+// ---------- Firebase configuration ---------- //
+
+var firebaseConfig = {
+    apiKey: "xxxx",
+    authDomain: "xxxx",
+    databaseURL: "xxxx",
+    projectId: "xxxx",
+    storageBucket: "xxxx",
+    messagingSenderId: "xxxx",
+    appId: "xxxx",
+    measurementId: "xxxx"
+};
+
+firebase.initializeApp(firebaseConfig); // Initialize Firebase
+firebase.auth.Auth.Persistence.LOCAL;
+
+// ----------------------------------------- //
